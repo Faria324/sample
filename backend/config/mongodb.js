c34d44a5bc812da69c507312f35cba3ba90d876d
Faria Fatima <fariafatima5678@gmail.com>
2026-01-31 11:57:45 +0500

    Solve the MongoDB connection error using WSL as terminal

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,15 +1,11 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  try {
-    mongoose.connection.on('connected', () =>
-      console.log("Database Connected")
-    );
 
-    await mongoose.connect(process.env.MONGODB_URI);
-  } catch (error) {
-    console.error("MongoDB connection failed:", error.message);
-  }
+        mongoose.connection.on('connected', () => console.log("Database Connected :)"))
+
+        await mongoose.connect(`${process.env.MONGODB_URI}/prescripto`)
+        
 };
 
 export default connectDB;
